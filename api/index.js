a1d2fb8e7fbf1efc48f809ea457871301ee3bcaa
Cliff Wright <clifford@cliffordwright.com>
2016-07-02 17:06:27 -0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,13 @@
+exports.register = (plugin, options, next) => {
+  plugin.route([
+    { method: 'GET', path: '/', config: Home.hello },
+    { method: 'GET', path: '/restricted', config: Home.restricted },
+    { method: 'GET', path: '/{path*}', config: Home.notFound }
+  ]);
+
+  next();
+};
+
+exports.register.attributes = {
+  name: 'api'
+}
